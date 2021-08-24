202b5a38d98b3cab8089563f352abf07800f86eb
Moffein <22742925+Moffein@users.noreply.github.com>
2021-08-24 12:12:34 -0700

    Spotter: DISRUPT no longer distracts bosses.

--- SniperClassic/SniperClassic.cs
+++ SniperClassic/SniperClassic.cs
@@ -29,7 +29,7 @@ namespace SniperClassic
     [R2API.Utils.R2APISubmoduleDependency(nameof(LanguageAPI), nameof(LoadoutAPI), nameof(PrefabAPI), nameof(SoundAPI), nameof(RecalculateStatsAPI), nameof(DamageAPI))]
     [BepInDependency("com.Kingpinush.KingKombatArena", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("com.DestroyedClone.AncientScepter", BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInPlugin("com.Moffein.SniperClassic", "Sniper Classic", "0.9.0")]
+    [BepInPlugin("com.Moffein.SniperClassic", "Sniper Classic", "0.9.1")]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
 
     public class SniperClassic : BaseUnityPlugin
