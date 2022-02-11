73f9ddfeb0886bf5d4f2c5634367644dec6aa1f9
Moffein <22742925+Moffein@users.noreply.github.com>
2022-02-11 14:44:50 -0700

    Re-added Language API for character unlockable text since it breaks without it.

--- SniperClassic/Modules/Tokens.cs
+++ SniperClassic/Modules/Tokens.cs
@@ -14,13 +14,19 @@ namespace SniperClassic.Modules
         {
             PhysicalFileSystem physicalFileSystem = new PhysicalFileSystem();
             Tokens.fileSystem = new SubFileSystem(physicalFileSystem, physicalFileSystem.ConvertPathFromInternal(System.IO.Path.GetDirectoryName(SniperClassic.pluginInfo.Location)), true);
-            if (Tokens.fileSystem.DirectoryExists("/language/")) //Uh, it exists and we make sure to not shit up R2Api
+            if (Tokens.fileSystem.DirectoryExists("/language/"))
             {
                 Language.collectLanguageRootFolders += delegate (List<DirectoryEntry> list)
                 {
                     list.Add(Tokens.fileSystem.GetDirectoryEntry("/language/"));
                 };
             }
+
+            //Need to figure out how to set up language file so that this isn't needed.
+            string increment = Achievements.CharacterUnlockAchievement.TestAchievementIncrement;
+            LanguageAPI.Add(increment + "SNIPERCLASSIC_CHARACTERUNLOCKABLE_ACHIEVEMENT_NAME", "Spotted");
+            LanguageAPI.Add(increment + "SNIPERCLASSIC_CHARACTERUNLOCKABLE_ACHIEVEMENT_DESC", "Repair an Equipment Drone with a Radar Scanner.");
+            LanguageAPI.Add(increment + "SNIPERCLASSIC_CHARACTERUNLOCKABLE_UNLOCKABLE_NAME", "Spotted");
         }
     }
 }
