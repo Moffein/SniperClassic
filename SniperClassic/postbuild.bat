6f1e1d0754aa621c69b57fd3ec0f92b299143e1d
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-31 21:48:22 -0700

    Fixed MP and fixed patcher.

--- SniperClassic/postbuild.bat
+++ SniperClassic/postbuild.bat
@@ -1,3 +1,3 @@
 rem weaver patch for mp compatibility
 cd Weaver\
-Unity.UNetWeaver.exe "..\libs\UnityEngine.CoreModule.dll" "..\libs\com.unity.multiplayer-hlapi.Runtime.dll" "..\bin\Debug\netstandard2.0" "..\bin\Debug\netstandard2.0\SniperClassic.dll" "..\libs"
+Unity.UNetWeaver.exe "..\libs\UnityEngine.CoreModule.dll" "..\libs\com.unity.multiplayer-hlapi.Runtime.dll" "..\bin\Debug\netstandard2.1" "..\bin\Debug\netstandard2.1\SniperClassic.dll" "..\libs"
